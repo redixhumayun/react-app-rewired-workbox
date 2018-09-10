c624ec681cad99f6e177d50f61f77c67f5de5a3c
Zaid Humayun <zaidhumayun@Zaids-MacBook-Pro.local>
2018-09-10 21:22:48 +0530

    repo working correctly

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,27 @@
+const workboxPlugin = require('workbox-webpack-plugin')
+const path = require('path')
+
 module.exports = {
-    webpack: function(config, env) {
-        return config
-    }, 
-    jest: function(config) {
-        return config
-    },
-    devServer: function(configFunction) {
-        return function (proxy, allowedHost) {
-            const config = configFunction(proxy, allowedHost)
-            return config
-        }
+  webpack: function (config, env) {
+    if (env === 'production') {
+      const workboxConfigProd = {
+        swSrc: path.join(__dirname, 'public', 'custom-service-worker.js'),
+        swDest: 'custom-service-worker.js',
+        importWorkboxFrom: 'disabled'
+      }
+      config = removeSWPrecachePlugin(config)
+      config.plugins.push(new workboxPlugin.InjectManifest(workboxConfigProd))
     }
+    return config
+  }
+}
+
+function removeSWPrecachePlugin (config) {
+  const swPrecachePluginIndex = config.plugins.findIndex((element) => {
+    return element.constructor.name === 'SWPrecacheWebpackPlugin'
+  })
+  if (swPrecachePluginIndex !== -1) {
+    config.plugins.splice(swPrecachePluginIndex, 1)
+  }
+  return config
 }
